5bbd2ea3964cf32f23fc26148b230a5a347e2999
jeff <jeff@ve12sus.com>
2021-02-11 17:46:33 -0500

    logged date from feeback.json

--- src/FeedbackApp.ts
+++ src/FeedbackApp.ts
@@ -1,7 +1,21 @@
 export class FeedbackApp {
-  formatFeedback(feedback : FeedBack) : string {
+  formatFeedback(feedback : FeedBack) : FeedBack {
     /* Start here */
-    return 'hello world';
+    console.log(typeof feedback.word);
+    console.log(feedback.word);
+    console.log(feedback.comment);
+    console.log(feedback.date);
+    console.log(typeof feedback.date);
+    const dateObjStr = feedback.date;
+    const instDate = new Date(dateObjStr);
+    const month = instDate.getMonth();
+    const day = instDate.getDate();
+    const year = instDate.getFullYear();
+    //  (9/19/2019)
+    const dateString = `(${month}/${day}/${year})`;
+
+    console.log(dateString);
+    return feedback;
   }
 }
 
