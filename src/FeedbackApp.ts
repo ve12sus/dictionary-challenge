4d732ce129ad82cf8fe7ba9213803f4103273d2e
jeff <jeff@ve12sus.com>
2021-02-12 12:59:56 -0500

    clearer outputs using template literals

--- src/FeedbackApp.ts
+++ src/FeedbackApp.ts
@@ -2,19 +2,27 @@ export class FeedbackApp {
   formatFeedback(feedback : FeedBack) : FeedBack {
     /* Start here */
     console.log(typeof feedback.word);
-    console.log(feedback.word);
     console.log(feedback.comment);
     console.log(feedback.date);
     console.log(typeof feedback.date);
-    const dateObjStr = feedback.date;
-    const instDate = new Date(dateObjStr);
+    const {
+      word, comment, date, rating,
+    } = feedback;
+    console.log(word);
+    console.log(comment);
+    console.log(date);
+    console.log(rating);
+    const instDate = new Date(date);
     const month = instDate.getMonth();
     const day = instDate.getDate();
     const year = instDate.getFullYear();
     //  (9/19/2019)
     const dateString = `(${month}/${day}/${year})`;
-
     console.log(dateString);
+    // swashbuckler: Yar, a good word, matey! Shiver me timbers! ★★★★½ (9/19/2019)
+
+    const finalString = `${word}: ${comment} ${dateString}`;
+    console.log(finalString);
     return feedback;
   }
 }
